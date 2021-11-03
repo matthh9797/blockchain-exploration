a96e6766db711ffdfac814aa5a5767b96ee9bca7
matthh9797 <matthewh@netextremity.com>
2021-11-03 14:27:18 +0000

    Added notebook and calls to bq to storage function

---
+++ bash/bq-to-storage-calls.sh
@@ -0,0 +1,5 @@
+python bq_to_storage.py bq_to_storage bitcoin-balance-query
+python bq_to_storage.py bq_to_storage bitcoin-zero-fee
+python bq_to_storage.py bq_to_storage ethereum-transacting-partner-count
+
+
